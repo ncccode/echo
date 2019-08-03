dc00beabc8e8f5b3ae6dba88f72a58492d22c5bd
宁采陈 <56694569@qq.com>
2019-08-04 01:18:06 +0800

    修复评论Undefined variable: group in报错问题

--- comments.php
+++ comments.php
@@ -1,7 +1,7 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
 <?php function threadedComments($comments, $options) {
-    $commentClass = '';
+    $commentClass = '';$group = '';
     if ($comments->authorId) {
         if ($comments->authorId == $comments->ownerId) {
             $group = '博主';
