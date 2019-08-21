3923abeea246e85259bcde8b0efc45fa1e604250
宁采陈 <56694569@qq.com>
2019-08-21 23:01:07 +0800

    兼容系统默认markdown排版样式等

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-layui.use(['layer', 'element'], function(){
+layui.use(['layer', 'element', 'util'], function(){
     var $ = layui.$,
     layer = layui.layer,
-    element = layui.element;
+    element = layui.element,
+    util = layui.util;
+    
     $(".nav-btn").on('click', function(){
         $('.nav-btn dl').toggleClass('layui-show');
     });
@@ -19,24 +21,30 @@ layui.use(['layer', 'element'], function(){
         }
     })
 
-    //文章图片点击事件
-    $(".text img").click(function() {
-        $.previewImage(this.src);
-    });
-    $.previewImage = function (src) {
-        var img = new Image(), index = layer.load(2, {time: 0, scrollbar: false, shade: [0.02, '#000']});
-        img.style.background = '#fff', img.style.display = 'none';
-        img.src = src;
-        document.body.appendChild(img), img.onerror = function () {
-            layer.close(index);
-        }, img.onload = function () {
-            layer.open({
-                type: 1, shadeClose: true, success: img.onerror, content: $(img), title: false,
-                area: img.width > 1140 ? '1140px' : img.width, closeBtn: 1, skin: 'layui-layer-nobg', end: function () {
-                    document.body.removeChild(img);
-                }
-            });
+    //文章图片点击事件(如果为pc端才生效)
+    var device = layui.device();
+    if(!(device.weixin || device.android || device.ios)){
+        $(".text img").click(function() {
+            $.previewImage(this.src);
+        });
+        $.previewImage = function (src) {
+            var img = new Image(), index = layer.load(2, {time: 0, scrollbar: false, shade: [0.02, '#000']});
+            img.style.background = '#fff', img.style.display = 'none';
+            img.src = src;
+            document.body.appendChild(img), img.onerror = function () {
+                layer.close(index);
+            }, img.onload = function () {
+                layer.open({
+                    type: 1, shadeClose: true, success: img.onerror, content: $(img), title: false,
+                    area: img.width > 1140 ? '1140px' : img.width, closeBtn: 1, skin: 'layui-layer-nobg', end: function () {
+                        document.body.removeChild(img);
+                    }
+                });
+            };
         };
-    };
+    }
+
+    //右下角工具箱（返回顶部）
+    util.fixbar();
     
 });
